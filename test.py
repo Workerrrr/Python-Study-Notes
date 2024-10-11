a78b7876b98fbcb6a595d9bfba50f96770fd2bc1
Workerrrr <a2271963917@outlook.com>
2024-10-11 21:16:29 +0800

    Dear Mei

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
-age = input("请输入年龄:")
-height = input("请输入身高:")
+date = ["7.24", "7.25", "7.26", "7.27", "7.28", "7.29"]  # 这是一个列表(list)
 
-age = int(age)
-height = int(height)
-
-if age <= 12:
-    if height <= 140:
-        print("儿童票")
-    else :
-        print("成人票")
+for value in date:
+    print(f"今天是{value}, 我向小美表白了!")
+else :
+    print("小美还是没有接受我的表白!")
