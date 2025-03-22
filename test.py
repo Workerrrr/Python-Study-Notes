05c561ca1f8b048fe84b6f27d0a06e0656a6d8e6
Workerrrr <a2271963917@outlook.com>
2025-03-22 13:28:55 +0800

    Update test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-with open(".\practice\essay.txt", "r", encoding = "UTF-8") as f:    # 打开文件
-    count : int = 0
-    for line in f:
-        for word in line.split():
-            if word == "to":
-                count += 1
-print(count)    # 输出为12
